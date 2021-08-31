28c7d5c1bfcaf05c5059259acac7730cc4ffc7b2
Santiago <Martnez>
2021-08-31 14:14:14 +0200

    lógica de programación pokédex

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import PreguntarPokemon from "./components/PreguntarPokemon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={"https://gist.githubusercontent.com/Galadirith/baaf38c7286b568973cc50a50ff57f4d/raw/34d60cae491bc505c212398b94f12705665c12fc/pokeball.svg"} className="App-logo" alt="logo" />
+          <PreguntarPokemon/>
       </header>
     </div>
   );
